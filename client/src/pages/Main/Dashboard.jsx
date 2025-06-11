73ca35f3bb712dd4da19585efb0e79a08edc260f
BLisme <tranbaolam006@gmail.com>
2025-06-11 23:38:46 +0700

    Able to save file to local

--- client/src/pages/Main/Dashboard.jsx
+++ client/src/pages/Main/Dashboard.jsx
@@ -4,7 +4,22 @@ import { UserContext } from "../../UserContext";
 export default function Dashboard() {
   const { token, setToken } = useContext(UserContext);
   if (!token) return <div>Loading...</div>;
-
+  //automatically send back information
+  async function userInfo() {
+    try {
+      const data = await fetch("/api/userinfo", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token.user.id}`,
+        },
+      });
+      data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  userInfo();
   return (
     <>
       <div>Hello, {token?.user?.user_metadata?.userName}</div>
@@ -16,9 +31,13 @@ export default function Dashboard() {
         Log out
       </button>
 
-      <form method="POST" action="/api/test">
+      <form
+        action="/api/fileuploading"
+        method="POST"
+        enctype="multipart/form-data"
+      >
         <h2>Upload your file</h2>
-        <label for="file">File:</label>
+        <label htmlFor="file">File:</label>
         <input id="file" type="file" name="file" accept=".csv, .xlsx"></input>
         <button type="submit">Submit your file</button>
       </form>
