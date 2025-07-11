7c4d384d358fcc547450bef2a285f83a9eba453d
BLisme <tranbaolam006@gmail.com>
2025-07-11 10:58:38 +0700

    even more fix

--- server/app.js
+++ server/app.js
@@ -12,21 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 // parse application/json ~ http request to object
 app.use(express.json());
 
-const hostname = "localhost";
-const port = 3000;
-let curUID; //userid
-
-//extract userid according to supabase
-// (can not catch if the server reloads so the server should not be changed during the time)
-// const { setUID, getUID } = require("./user_data/userInfo.js");
-// app.post("/userinfo", (req, res) => {
-//   //get user infomation
-//   const token = req.headers["authorization"]?.split(" ")[1]; // "Bearer <token>"
-//   setUID(token || "");
-//   console.log(getUID() + " is login");
-//   res.json({ message: " Get data completed" });
-// });
-// module.exports = curUID;
+const port = process.env.PORT || 3000;
 
 //File system control
 const FileRouter = require("./routers/FileRouter");
