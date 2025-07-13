886f7d573dc1290d5fee15bb35f0de0a3c5215fe
Minh Nhat Pham <nhatminh.pham.743@gmail.com>
2025-07-13 20:01:57 +0700

    Remove file for development only

--- client/src/client.js
+++
@@ -1,9 +0,0 @@
-//file for supabase access
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
-);
-
-export default supabase;
--- server/controllers/AnalyticController.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const { getUID } = require("../user_data/userInfo.js");
-const getMulterUpload = require("../middlewares/MulterConfig.js");
-
-const showHeader = async (req, res, next) => {};
-
-module.exports = {
-  showHeader,
-};
